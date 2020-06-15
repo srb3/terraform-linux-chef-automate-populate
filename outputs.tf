4615ea83a02cc46d1aeeaa83d9a603e7aec295ea
srb3 <sbrown@chef.io>
2020-06-15 16:41:14 +0100

    remove legacy module hook

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,6 @@ output "server_public_ip" {
   value = [var.ips]
 }
 
-output "module_hook" {
-  description = "A list of random string outputs from each connection this module made. Used to enforce the order in which the module is run"
-  value       = data.null_data_source.module_hook.*.outputs.data
-}
-
-output "admin_token" {
+output "ingest_token" {
   value = data.external.populate_data[*].result["token"]
 }
